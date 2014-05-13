fd7eaf4646c0457006abc7b5943bf3800bf935c3
littlekev34 <littlekev34@hotmail.fr>
2014-05-13 16:24:34 +0200

    mise a jour

---
+++ SGBD_LIBRARY/src/DAL/DAOPartenairesPatient.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package DAL;
+
+import BOL.Patient;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Kevin
+ */
+public class DAOPartenairesPatient implements IBdd{
+
+    @Override
+    public void insert(Object objet) {
+    }
+
+    @Override
+    public void update(Object objet) {
+    }
+
+    @Override
+    public void delete(Object objet) {
+    }
+
+    @Override
+    public Patient selectOne(int id) {
+        Patient patient = new Patient();
+        return patient;
+    }
+
+    @Override
+    public ArrayList<Object> selectAll(Object objet) {
+        ArrayList<Object> retour = new ArrayList<Object>();
+        return retour;
+    }
+    
+}
