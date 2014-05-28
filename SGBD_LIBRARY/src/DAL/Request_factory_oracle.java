41f8885fb12a485db73896039b95a33dc8761b4a
stefhack <duprestephane@hotmail.fr>
2014-05-28 12:40:45 +0200

    Tests update ok

--- SGBD_LIBRARY/src/DAL/Request_factory_oracle.java
+++ SGBD_LIBRARY/src/DAL/Request_factory_oracle.java
@@ -192,9 +192,11 @@ public class Request_factory_oracle implements IBDD
         //Construction d'un arrayList d'attributs sous forme de string
         //Pour les utiliser dans la construction de la requete
         ArrayList<String> fieldsString = getFieldstoString(fields);
+      
         int compteur = 0;
         for (String field : fieldsString)
           {
+              
             String nomMethode = "get_" + field;
             if (!field.subSequence(0, 2).equals("id")) //On écarte l'id PAR CONTRE A REVOIR
               {
