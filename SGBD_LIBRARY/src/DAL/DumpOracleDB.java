6bfca0435cd4b84b607b1b043ba46ee43102cf8d
JohnnyBeGood34 <jonathanaffre@gmail.com>
2014-06-01 14:22:17 +0200

    dump db

--- SGBD_LIBRARY/src/DAL/DumpOracleDB.java
+++ SGBD_LIBRARY/src/DAL/DumpOracleDB.java
@@ -4,66 +4,66 @@
  */
 package DAL;
 
-import MAIN.Menu;
-import MAIN.SGBD_LIBRARY;
 import java.sql.Connection;
-import java.sql.DatabaseMetaData;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  * Classe permettant de faire un export de la base de données oracle
+ *
  * @author JOHN
  */
 public class DumpOracleDB
   {
-    
-      //Get instance du singleton de connexion
-      private  Connection connexion = Oracle_connexion.getInstance();
-    public static String dumpOracleDB()
+
+    //Get instance du singleton de connexion
+    private Connection connexion = Oracle_connexion.getInstance();
+
+    public String dumpOracleDB() throws SQLException
       {
         //Requete renvoyant le nom des tables de la base de données
         //select dbms_metadata.get_ddl('TABLE','MATERIELMEDICAL')from dual
-        String requeteTables = "SELECT table_name FROM user_tables;";
-        
+        ArrayList<String> listeTables = listerTables();
         return "yoyo les couilles a Jeano";
       }
-    
-   /**
-    * Methode retournant la liste des Tables de la BD
-    * @param connection type Connection, la connection pour la BD
-    * @return ArrayList<String> la liste des tables de la BD
+
+    /**
+     * Methode retournant la liste des Tables de la BD
+     *
+     * @param connection type Connection, la connection pour la BD
+     * @return ArrayList<String> la liste des tables de la BD
      * @throws java.sql.SQLException
-    */
-    public ArrayList<String> listerTables(Connection connection) throws SQLException{
-        
-        ArrayList<String> listeTables=new ArrayList<>();
-        Statement statement=connection.createStatement();
-        
+     */
+    private ArrayList<String> listerTables() throws SQLException
+      {
+
+        ArrayList<String> listeTables = new ArrayList<>();
+        Statement statement = connexion.createStatement();
+
         /* Requête permettant de récupérer tous les noms de table */
-        ResultSet resultSet=statement.executeQuery("SELECT table_name FROM user_tables");
-        
-        while(resultSet.next()){
-            String nomTable=resultSet.getString("TABLE_NAME");
-            listeTables.add(nomTable);            
-        }
-        
+        ResultSet resultSet = statement.executeQuery("SELECT table_name FROM user_tables");
+
+        while (resultSet.next())
+          {
+            String nomTable = resultSet.getString("TABLE_NAME");
+            listeTables.add(nomTable);
+          }
+
         return listeTables;
-    }
-    
+      }
+
     /**
      * Méthode retournant la liste des colonnes d'une table
+     *
      * @param table type String, nom de la table
-     * @return ArrayList<String> 
-    */
-    public ArrayList<String> listerColonnes(String table){
-        ArrayList<String> listeColonnes=new ArrayList();
-        
+     * @return ArrayList<String>
+     */
+    public ArrayList<String> listerColonnes(String table)
+      {
+        ArrayList<String> listeColonnes = new ArrayList();
+
         return listeColonnes;
-    }
-    }
-  
+      }
+  }
