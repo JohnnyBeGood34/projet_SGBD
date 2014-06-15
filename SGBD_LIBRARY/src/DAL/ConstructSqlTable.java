43602e5d1375873df92e05d864957ec1ebdd9a7c
JohnnyBeGood34 <jonathanaffre@gmail.com>
2014-06-15 14:28:03 +0200

    update build.xml

--- SGBD_LIBRARY/src/DAL/ConstructSqlTable.java
+++ SGBD_LIBRARY/src/DAL/ConstructSqlTable.java
@@ -52,20 +52,82 @@ public class ConstructSqlTable implements Runnable
             System.out.println("TRY TRY TRY MOTHER FUCKER STP MARCHE");
             ArrayList<String> listeTables = new ArrayList<>();
             /* Requête permettant de récupérer tous les noms de table */
-            try (ResultSet resultSet = statement.executeQuery("SELECT table_name FROM user_tables"))
+
+            /* Requête permettant de récupérer tous les noms de table */
+            ResultSet resultSet = statement.executeQuery("SELECT table_name FROM user_tables");
+            while (resultSet.next())
               {
-                System.out.println(resultSet.toString());
-                while (resultSet.next())
-                  {
-                    String nomTable = resultSet.getString("TABLE_NAME");
-                    listeTables.add(nomTable);
-                  }
+                String nomTable = resultSet.getString("TABLE_NAME");
+                listeTables.add(nomTable);
               }
-            System.out.println(listeTables);
-            
+            //String builder permettra de récupérer la chaine à retourner par la focntion
+            StringBuilder sBuilderTable = new StringBuilder();
+
+            String entete = "-------------------\r\n";
+            entete += "--Dump de la base--\r\n";
+            entete += "------------------\r\n\n\n";
+
+            String creation = "";
+            String insertion = "\r\n\n";
+            //Pour chaque tables
+            for (String table : listeTables)
+              {
+                creation += "-- -----------------\r\n";
+                creation += "-- creation de la table --" + table + " --\r\n";
+                creation += "----------------------\r\n";
+                // Requête permettant de récupérer tous les create table 
+                ResultSet resultSetTable = statement.executeQuery("select dbms_metadata.get_ddl('TABLE','" + table + "')from dual");
+
+                //Pour chaques create table
+                while (resultSetTable.next())
+                  {
+                    //On récupère le create table et on le concatene
+                    creation += resultSetTable.getString(1) + "\r\n\n";
+                  }
+                //Récupération des enregistrements de la table en question
+                ResultSet resultSetSelect = statement.executeQuery("SELECT * FROM " + table);
+                insertion += "-- -------------\r\n";
+                insertion += "-- insertion dans la table " + table + " --\r\n";
+                insertion += "---------------------\r\n";
+                //Pour chaque enregistrements
+                ResultSetMetaData meta = resultSetSelect.getMetaData();
+                int nbColonnes = meta.getColumnCount();
+                while (resultSetSelect.next())
+                  {
+                    insertion += "INSERT INTO " + table + " VALUES(";
+                    for (int compteur = 1; compteur < nbColonnes; compteur++)
+                      {
+                        if (compteur != 1)
+                          {
+                            insertion += ",";
+                          }
+                        insertion += "'" + resultSetSelect.getString(compteur) + "'";
+                      }
+                    insertion += ");\r\n";
+                  }
+                insertion += "\r\n";
+              }
+
+            sBuilderTable.append(entete).append("\n").append(creation).append("\n").append(insertion);
+              System.out.println(sBuilderTable.toString());
           } catch (SQLException ex)
           {
             Logger.getLogger(ConstructSqlTable.class.getName()).log(Level.SEVERE, null, ex);
           }
+      }
+
+    public void writeDumpFile(String contenu, String chemin) throws IOException
+      {
+
+        Date date = new Date();
+        String dateFichier = new SimpleDateFormat("dd-MM-yyyy").format(date);
+        File fichier = new File(chemin + "dump_" + dateFichier + ".sql");
+
+        FileWriter filewriter = new FileWriter(fichier);
+        try (BufferedWriter bufferwriter = new BufferedWriter(filewriter))
+          {
+            bufferwriter.write(contenu);
+          }
+
       }
   }
